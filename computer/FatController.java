9c55dfdcdc9819f8e5bb5993b7a4bbeba9b0c31d
Oetzi <oetzi101@gmail.com>
2011-01-17 17:42:24 +0000

    Added FatController

---
+++ computer/FatController.java
@@ -0,0 +1,36 @@
+import lejos.pc.comm.*;
+import java.io.*;
+
+public class FatController
+{
+	public static void main(String[] args)
+	{
+		NXTComm nxtComm = null;
+
+		try
+		{
+			nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);
+			NXTInfo nxtInfo = new NXTInfo(NXTCommFactory.BLUETOOTH, "WAR BASTARD", "00:16:53:0A:07:1D");
+			nxtComm.open(nxtInfo);
+		}
+
+		catch (NXTCommException e)
+		{
+			e.printStackTrace();
+		}
+		
+
+		OutputStream stream = nxtComm.getOutputStream();
+
+		try
+		{
+			stream.write(2);
+			stream.close();	
+		}
+
+		catch (IOException e)
+		{
+			System.out.println("IOException");
+		}
+	}
+}
