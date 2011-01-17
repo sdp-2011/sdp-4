d9d634ce26d0dfacd14474178e27fa48f5309b45
Oetzi <oetzi101@gmail.com>
2011-01-17 17:17:49 +0000

    Made marvin remote. :)

--- robot/src/Marvin.java
+++ robot/src/Marvin.java
@@ -1,15 +1,42 @@
 import lejos.nxt.*;
+import lejos.nxt.comm.*;
+import java.io.*;
+
 
 public class Marvin
 {
 	public static void main(String [] args) throws InterruptedException
 	{
 		Robot robot = new Robot();
-		robot.drive(Robot.Direction.FORWARD);
-		Thread.sleep(500);
-		robot.reverseDirection();
-		Thread.sleep(500);
-		robot.stop();
-		Button.waitForPress();
+		NXTConnection blueT = Bluetooth.waitForConnection();
+		DataInputStream stream = blueT.openDataInputStream();
+
+		while (true)
+		{
+			try
+			{
+				byte data = stream.readByte();
+
+				if (data == 0)
+				{
+					robot.drive(Robot.Direction.FORWARD);
+				}
+
+				else if (data == 1)
+				{
+					robot.drive(Robot.Direction.BACKWARD);
+				}
+
+				else if (data == 2)
+				{
+					robot.shoot();
+				}
+			}
+
+			catch (IOException e)
+			{
+				System.exit(0);
+			}
+		}
 	}
 }
