25a6bcf813dee6f33cfe1cb65f6261e30215f73a
Chris Brown <cb@tardis.ed.ac.uk>
2011-01-17 15:32:51 +0000

    Added some sample code to test the robot.

--- robot/src/Marvin.java
+++ robot/src/Marvin.java
@@ -1,7 +1,15 @@
+import lejos.nxt.*;
+
 public class Marvin
 {
-	public static void main(String [] args)
+	public static void main(String [] args) throws InterruptedException
 	{
 		Robot robot = new Robot();
+		robot.drive(Robot.Direction.FORWARD);
+		Thread.sleep(500);
+		robot.reverseDirection();
+		Thread.sleep(500);
+		robot.stop();
+		Button.waitForPress();
 	}
 }
