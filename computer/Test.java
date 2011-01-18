70df4d0ba32442c48c3ce0be68d15520e77740ab
Oetzi <oetzi101@gmail.com>
2011-01-18 20:02:00 +0000

    Forgot to actually add Test.java. Derp.

---
+++ computer/Test.java
@@ -0,0 +1,11 @@
+public class Test
+{
+	public static void main(String[] args)
+	{
+		FatController fatty = new FatController();
+		fatty.drivef(40);
+		fatty.driveb(20);
+		fatty.shoot();
+		fatty.finish();
+	}
+}
