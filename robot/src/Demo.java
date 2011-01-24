6195fae6353a116ae13689e90117fb6303e83384
Oetzi <oetzi101@gmail.com>
2011-01-24 18:30:12 +0000

    Changed Demo back

--- robot/src/Demo.java
+++ robot/src/Demo.java
@@ -3,11 +3,13 @@ import java.io.*;
 
 public class Demo
 {
-	public static void newBuild(Robot robot) throws InterruptedException
+	public static void main(String [] args) throws InterruptedException
 	{
+		Robot robot = new Robot();
 		robot.drive(10.0f, false);
-		robot.drive(-10.0f, false);
 		robot.shoot();
 		Button.waitForPress();
 	}
 }
+
+
