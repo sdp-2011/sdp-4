7bbc4e7bf2a179ebda3870cef85696e13f6de11f
Steven <thepars@gmail.com>
2011-01-28 15:09:59 +0000

    Refactor python vision script

--- vision/src/python/opencvpython.py
+++ vision/src/python/opencvpython.py
@@ -1,12 +1,26 @@
 import cv
 
 
-def process(img, colour):
+def findObject(img, colour):
+    '''
+    Finds the objects in an image with given colour.
+
+    Arguments:
+    img	    -- the image to be processed
+    colour  -- the colour to look for (red, blue or yellow)
+
+    Returns:
+    Attributes of bounding rectangle around an object
+
+    '''
+
     # Convert to hsv
     size = cv.GetSize(img)
-    tempImage = cv.CreateImage((size[0] / 2,  size[1] / 2), 8, 3)
-    cv.PyrDown(img, tempImage, 7)  # Get rid of noise
-    cv.PyrUp(tempImage, img, 7)   # by de- and up-scaling
+    tempImage = cv.CreateImage((size[0] / 2, size[1] / 2), 8, 3)
+
+    # Reduce noise by down- and up-scaling input image
+    cv.PyrDown(img, tempImage, 7)
+    cv.PyrUp(tempImage, img, 7)
     hsv = cv.CreateImage(size, cv.IPL_DEPTH_8U, 3)
     cv.CvtColor(img, hsv, cv.CV_BGR2HSV)
 
@@ -14,20 +28,25 @@ def process(img, colour):
     mask = cv.CreateMat(size[1], size[0], cv.CV_8UC1)
     maskSize = cv.GetSize(mask)
     if (colour == "RED"):
-        cv.InRangeS(hsv, cv.Scalar(0.00 * 256, 0.50 * 256, 0.50 * 256),
-                cv.Scalar(0.05 * 256, 1.00 * 256, 1.00 * 256), mask)
-    elif (colour == "BLUE"):
-        cv.InRangeS(hsv, cv.Scalar(0.30 * 256, 0.30 * 256, 0.30 * 256),
-                cv.Scalar(0.80 * 256, 1.00 * 256, 1.00 * 256), mask)
-    elif (colour == "YELLOW"):
-        cv.InRangeS(hsv, cv.Scalar(0.10 * 256, 0.45 * 256, 0.50 * 256),
-                cv.Scalar(0.20 * 256, 1.00 * 256, 1.00 * 256), mask)
-        cv.NamedWindow("Yellow mask:", cv.CV_WINDOW_AUTOSIZE)
-        cv.ShowImage("Yellow mask:", mask)
-    # Make sure code doesn't segfault if it doesn't find anything
-    if (cv.CountNonZero(mask) < 3):
-        return (0, 0, 0, 0)
+	redLower = cv.Scalar(0.00 * 256, 0.50 * 256, 0.50 * 256)
+	redUpper = cv.Scalar(0.05 * 256, 1.00 * 256, 1.00 * 256)
+	cv.InRangeS(hsv, redLower, redUpper, mask)
 
+    elif (colour == "BLUE"):
+    	blueLower = cv.Scalar(0.30 * 256, 0.30 * 256, 0.30 * 256)
+	blueUpper = cv.Scalar(0.80 * 256, 1.00 * 256, 1.00 * 256)
+	cv.InRangeS(hsv, blueLower, blueUpper, mask)
+
+    elif (colour == "YELLOW"):
+	yellowLower = cv.Scalar(0.10 * 256, 0.45 * 256, 0.50 * 256)
+	yellowUpper = cv.Scalar(0.20 * 256, 1.00 * 256, 1.00 * 256)
+	cv.InRangeS(hsv, yellowLower, yellowUpper, mask)
+
+    # Count white pixels to make sure program doesn't crash if it finds nothing
+    if (cv.CountNonZero(mask) < 3):
+	    return (0, 0, 0, 0)  # (0, 0, 0, 0) is a rectangle with no attributes
+
+    # Clean up the image to reduce anymore noise in the binary image
     cv.Smooth(mask, mask, cv.CV_GAUSSIAN, 9, 9, 0, 0)
     convKernel = cv.CreateStructuringElementEx(1, 1, 0, 0, cv.CV_SHAPE_ELLIPSE)
     cv.Erode(mask, mask, convKernel, 1)
@@ -39,40 +58,55 @@ def process(img, colour):
     cv.Copy(mask, contourImage, None)
 
     storage = cv.CreateMemStorage()
-
-    contours = cv.FindContours(contourImage, storage, cv.CV_RETR_EXTERNAL,
-            cv.CV_CHAIN_APPROX_SIMPLE)
+    contours = cv.FindContours(contourImage, storage, cv.CV_RETR_EXTERNAL, cv.CV_CHAIN_APPROX_SIMPLE)
 
     if len(contours) > 1:
-        contourLow = cv.ApproxPoly(contours, storage, cv.CV_POLY_APPROX_DP)
-        rect = cv.BoundingRect(contourLow, 0)
-        return rect
+	    contourLow = cv.ApproxPoly(contours, storage, cv.CV_POLY_APPROX_DP)
+	    rect = cv.BoundingRect(contourLow, 0)
+	    return rect
+
     return (0, 0, 0, 0)
 
-cv.NamedWindow("Original:", cv.CV_WINDOW_AUTOSIZE)
-cv.NamedWindow("Processed:", cv.CV_WINDOW_AUTOSIZE)
-cam = cv.CaptureFromCAM(0)
 
-while (True):
-    image = cv.QueryFrame(cam)
-    cropRect = (75, 80, 554, 340)
-    roi = cv.SetImageROI(image, cropRect)
-    orig = cv.CloneImage(image)
-    processed = cv.CloneImage(orig)
-    ballRect = process(image, "RED")
-    blueRect = process(image, "BLUE")
-    yellowRect = process(image, "YELLOW")
+def main():
+    '''
+    Continually grabs new frame from camera and looks for each object.
+    
+    '''
 
-    cv.Rectangle(processed, (ballRect[0], ballRect[1]),
-            (ballRect[0] + ballRect[2], ballRect[1] + ballRect[3]),
-            cv.RGB(255, 0, 0), 1, 8, 0)
-    cv.Rectangle(processed, (blueRect[0], blueRect[1]),
-            (blueRect[0] + blueRect[2], blueRect[1] + blueRect[3]),
-            cv.RGB(0, 0, 255), 1, 8, 0)
-    cv.Rectangle(processed, (yellowRect[0], yellowRect[1]),
-            (yellowRect[0] + yellowRect[2], yellowRect[1] + yellowRect[3]),
-            cv.RGB(255, 255, 0), 1, 8, 0)
-    cv.ShowImage("Original:", orig)
-    cv.ShowImage("Processed:",  processed)
+    cv.NamedWindow("Original:", cv.CV_WINDOW_AUTOSIZE)
+    cv.NamedWindow("Processed:", cv.CV_WINDOW_AUTOSIZE)
+    cam = cv.CaptureFromCAM(0)
 
-    cv.WaitKey(30)
+    while (True):
+	image = cv.QueryFrame(cam)
+	cropRect = (75, 80, 554, 340)
+	roi = cv.SetImageROI(image, cropRect)
+	orig = cv.CloneImage(image)
+	processed = cv.CloneImage(orig)
+
+	ballRect = process(image, "RED")
+	blueRect = process(image, "BLUE")
+	yellowRect = process(image, "YELLOW")
+
+	ballTL = (ballRect[0], ballRect[1])
+	ballBR = (ballRect[0] + ballRect[2], ballRect[1] + ballRect[3])
+
+	blueTL = (blueRect[0], blueRect[1])
+	blueBR = (blueRect[0] + blueRect[2], blueRect[1] + blueRect[3])
+
+	yellowTL = (yellowRect[0], yellowRect[1])
+	yellowBR = (yellowRect[0] + yellowRect[2], yellowRect[1] + yellowRect[3])
+
+	cv.Rectangle(processed, ballTL, ballBR, cv.RGB(255, 0, 0), 1, 8, 0)
+	cv.Rectangle(processed, blueTL, blueBR, cv.RGB(0, 0, 255), 1, 8, 0)
+	cv.Rectangle(processed, yellowTL, yellowBR, cv.RGB(255, 255, 0), 1, 8, 0)
+
+	cv.ShowImage("Original:", orig)
+	cv.ShowImage("Processed:", processed)
+
+	cv.WaitKey(30)
+
+
+if __name__ == "__main__":
+	main()
