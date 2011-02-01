e24ec32d3547c12e2040f7d948ac154442ad361b
Chris Brown <cb@tardis.ed.ac.uk>
2011-02-01 16:48:28 +0000

    Initial implementation. FUCK YEAH

---
+++ share/protobuffer/java/uk/ac/ed/inf/sdp/group4/run.sh
@@ -0,0 +1,2 @@
+java -cp .:protobuf-java-2.3.0.jar Main uk.ac.ed.inf.sdp.group4.Worldstate$WorldStateRequest #benchmarks.GoogleSize$SizeMessage1 ../google_message1.dat benchmarks.GoogleSpeed$SpeedMessage1 ../google_message1.dat benchmarks.GoogleSize$SizeMessage2 ../google_message2.dat benchmarks.GoogleSpeed$SpeedMessage2 ../google_message2.dat
+
