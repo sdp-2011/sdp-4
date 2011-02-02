72a2aea11054e1b6aa2ad315eca9821bd29e31e2
Steven Skelton <thepars@gmail.com>
2011-02-02 16:55:04 +0000

    Add calculation of rotation

--- vision/src/python/main.py
+++ vision/src/python/main.py
@@ -1,4 +1,4 @@
-git puimport cv
+import cv
 from imageProcFunctions import *
 from navigation import *
 
@@ -14,24 +14,35 @@ while (True):
 	orig = cv.CloneImage(image)
 	processed = cv.CloneImage(orig)
 
-	ballCenter = findObject(image, "RED")
-	blueCenter = findObject(image, "BLUE")
-	yellowCenter = findObject(image, "YELLOW")
+	ballCenter = findObject(image,"RED")
+	ballCenter = (int(ballCenter[0]), int(ballCenter[1]))
+	blueCenter = findObject(image,"BLUE")
+	blueCenter = (int(blueCenter[0]), int(blueCenter[1]))
+	yellowCenter = findObject(image,"YELLOW")
+	yellowCenter = (int(yellowCenter[0]), int(yellowCenter[1]))
 	
-	blueImage = cv.CloneImage(orig)
-	blueCropRect = (blueCenter[0] - 30, blueCenter[1] - 40, 90, 80)
+	blueImage = cv.CloneImage(orig)	
+	blueCropRect = (blueCenter[0] + 50, blueCenter[1] + 55, 50, 50)
+	cv.SetImageROI(blueImage, blueCropRect)
+	cv.NamedWindow("BlueBlack:",cv.CV_WINDOW_AUTOSIZE)
+	cv.ShowImage("BlueBlack:", blueImage)	
 	blueBlack = findObject(blueImage, "BLACK")
+	blueBlack = (int(blueBlack[0]) + blueCropRect[0], int(blueBlack[1]) + blueCropRect[1])	
 	
 	yellowImage = cv.CloneImage(orig)
-	yellowCropRect = (yellowCenter[0] - 30, yellowCenter[1] - 40, 90, 80)
+	yellowCropRect = (yellowCenter[0] + 50, yellowCenter[1] + 55, 50, 50)
+	cv.SetImageROI(yellowImage, yellowCropRect)
+	cv.NamedWindow("YellowBlack:",cv.CV_WINDOW_AUTOSIZE)
+	cv.ShowImage("YellowBlack:", yellowImage)
 	yellowBlack = findObject(yellowImage, "BLACK")
+	yellowBlack = (int(yellowBlack[0]) + yellowCropRect[0], int(yellowBlack[1]) + yellowCropRect[1])
 		
-	cv.Circle(processed, (int(ballCenter[0]), int(ballCenter[1])), 2, cv.RGB(0,0,0),-1)	
-	cv.Circle(processed, (int(blueCenter[0]), int(blueCenter[1])), 2, cv.RGB(0,0,0),-1)	
-	cv.Circle(processed, (int(yellowCenter[0]), int(yellowCenter[1])), 2, cv.RGB(0,0,0),-1)
+	cv.Circle(processed, ballCenter, 2, cv.RGB(0,0,0),-1)	
+	cv.Circle(processed, blueCenter, 2, cv.RGB(0,0,0),-1)	
+	cv.Circle(processed, yellowCenter, 2, cv.RGB(0,0,0),-1)
 	
-	cv.Line(processed, blueBlack, blueCenter)
-	cv.Line(processed, yellowBlack, yellowCenter)
+	cv.Line(processed, (blueBlack[0] - 75, blueBlack[1] - 80), blueCenter, cv.RGB(255,0,0))
+	cv.Line(processed, (yellowBlack[0] - 75, yellowBlack[1] - 80) , yellowCenter, cv.RGB(0,255,0))
 	
 	cv.ShowImage("Original:", orig)
 	cv.ShowImage("Processed:", processed)
