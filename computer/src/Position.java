fbe07545fd9b35466c1e803147a90cdd175bb420
Chris Brown <cb@tardis.ed.ac.uk>
2011-02-02 16:30:26 +0000

    Created some basic domain geometry classes.

---
+++ computer/src/Position.java
@@ -0,0 +1,27 @@
+package uk.ac.ed.inf.sdp.group4.domain;
+
+class Position
+{
+	private int x;
+	private int y;
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+
+	public int getX()
+	{
+		return this.x;
+	}
+	
+	public int getY()
+	{
+		return this.y;
+	}
+}
--- computer/src/Vector.java
+++ computer/src/Vector.java
@@ -1,5 +1,40 @@
+package uk.ac.ed.inf.sdp.group4.domain;
+
 class Vector
 {
 	private int direction;
 	private float magnitude;
+
+	public Vector(int direction, float magnitude)
+	{
+		setDirection(direction);
+		setMagnitude(magnitude);
+	}
+
+	public void setDirection(int direction) throws InvalidAngleException
+	{
+		if (direction < 0 || direction > 359)
+		{
+			throw new InvalidAngleException(direction);
+		}
+		else
+		{
+			this.direction = direction;
+		}
+	}
+
+	public void setMagnitude(float magnitude)
+	{
+		this.magnitude = magnitude;
+	}
+
+	public int getDirection()
+	{
+		return this.direction;
+	}
+	
+	public float getMagnitude()
+	{
+		return this.magnitude;
+	}
 }
