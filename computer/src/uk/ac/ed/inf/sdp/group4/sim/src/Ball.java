63ff31b0c87e81cf90c8c3c3707917b5f160746f
Oetzi <oetzi101@gmail.com>
2011-02-02 18:38:18 +0000

    Fixed exception throws

---
+++ computer/src/uk/ac/ed/inf/sdp/group4/sim/src/Ball.java
@@ -0,0 +1,9 @@
+
+public class Ball {
+
+	final double radius = 2.1335;
+	
+	private double xSpeed = 0;
+	private double ySpeed = 0;
+		
+}
--- computer/src/uk/ac/ed/inf/sdp/group4/world/Ball.java
+++ computer/src/uk/ac/ed/inf/sdp/group4/world/Ball.java
@@ -1,10 +1,11 @@
 package uk.ac.ed.inf.sdp.group4.world;
 
 import uk.ac.ed.inf.sdp.group4.world.WorldObject;
+import  uk.ac.ed.inf.sdp.group4.domain.InvalidAngleException;
 
 class Ball extends WorldObject
 {
-	public Ball(int x, int y, int direction, float speed)
+	public Ball(int x, int y, int direction, float speed) throws InvalidAngleException
 	{
 		setPosition(x, y);
 		setVector(direction, speed);
