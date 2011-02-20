3849b3afd12c8b8c08a8f148f7ead00102f9eccb
Steven Skelton <thepars@gmail.com>
2011-02-20 16:00:49 +0000

    Continued refactoring

--- vision/src/main.py
+++ vision/src/main.py
@@ -7,6 +7,7 @@ from worldstate import *
 from setup import *
 
 def draw_on_image(image, object_descriptors):
+
     cv.Circle(image, object_descriptors[0][0], 2, cv.RGB(0,0,0),-1)	
     cv.Circle(image, object_descriptors[1][0], 2, cv.RGB(0,0,0),-1)	
     cv.Circle(image, object_descriptors[2][0], 2, cv.RGB(0,0,0),-1)
@@ -30,7 +31,10 @@ def update_worldstate(object_descriptors):
 setup_system()
 cam = cv.CaptureFromCAM(0)
 
+last_time = 0
+
 while (True):
+    
     start = time.time()
     image = cv.QueryFrame(cam)
     cropRect = (75, 80, 554, 325)
@@ -39,10 +43,15 @@ while (True):
     processed = cv.CloneImage(orig)
     
     object_descriptors = (find_object(image,"RED"), find_object(image,"BLUE"), find_object(image,"YELLOW"))
+    print "Blue orientation: ", object_descriptors[1][1]
+    print "Yellow orientation:", object_descriptors[2][1]
     
     draw_on_image(processed, object_descriptors)
     
     update_worldstate(object_descriptors)
     
+    end = time.time()
+    fps = 0.9 * (1/(end-start)) + 0.1 * last_time
+    print "FPS:", fps
+    last_time = fps 
     cv.WaitKey(25)
-	
