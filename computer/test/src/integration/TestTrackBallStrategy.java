ed3b455cca94f34b06f1c2162475d5f8f5e7a6e7
Chris Brown <cb@tardis.ed.ac.uk>
2011-02-21 01:36:59 +0000

    Added more tests.

--- computer/test/src/integration/TestTrackBallStrategy.java
+++ computer/test/src/integration/TestTrackBallStrategy.java
@@ -62,5 +62,68 @@ public class TestTrackBallStrategy
 		// Did we shoot?
 		assertEquals(2, testController.getCommand());
 	}
+
+	@Test
+	public void testSimpleTurnRight() throws InvalidAngleException, BadWorldStateException
+	{
+		// Set up the world state.
+		Ball ball = new Ball(400, 160, 0, 0.0f);
+		Robot blue = new Robot(320, 160, 0, 0.0f, 0.0f, RobotColour.BLUE);
+		Robot yellow = new Robot(0, 0, 0, 0.0f, 0.0f, RobotColour.YELLOW);
+		WorldState state = new WorldState(ball, blue, yellow);
+
+		// A fake vision client to report this.
+		visionClient = new TestVisionClient(state);
+		
+		// Run an iteration
+		trackBallStrategy = new TrackBallStrategy(visionClient, testController, RobotColour.BLUE);
+		trackBallStrategy.tick();
+
+		// Did we turn?
+		assertEquals(5, testController.getCommand());
+		assertEquals(90, testController.getArgument());
+	}
+
+	@Test
+	public void testSimpleTurnLeft() throws InvalidAngleException, BadWorldStateException
+	{
+		// Set up the world state.
+		Ball ball = new Ball(400, 160, 0, 0.0f);
+		Robot blue = new Robot(320, 160, 0, 0.0f, 180.0f, RobotColour.BLUE);
+		Robot yellow = new Robot(0, 0, 0, 0.0f, 0.0f, RobotColour.YELLOW);
+		WorldState state = new WorldState(ball, blue, yellow);
+
+		// A fake vision client to report this.
+		visionClient = new TestVisionClient(state);
+		
+		// Run an iteration
+		trackBallStrategy = new TrackBallStrategy(visionClient, testController, RobotColour.BLUE);
+		trackBallStrategy.tick();
+
+		// Did we turn?
+		assertEquals(4, testController.getCommand());
+		assertEquals(90, testController.getArgument());
+	}
+
+	@Test
+	public void testDriveForwards() throws InvalidAngleException, BadWorldStateException
+	{
+		// Set up the world state.
+		Ball ball = new Ball(400, 160, 0, 0.0f);
+		Robot blue = new Robot(200, 160, 0, 0.0f, 90.0f, RobotColour.BLUE);
+		Robot yellow = new Robot(0, 0, 0, 0.0f, 0.0f, RobotColour.YELLOW);
+		WorldState state = new WorldState(ball, blue, yellow);
+
+		// A fake vision client to report this.
+		visionClient = new TestVisionClient(state);
+		
+		// Run an iteration
+		trackBallStrategy = new TrackBallStrategy(visionClient, testController, RobotColour.BLUE);
+		trackBallStrategy.tick();
+
+		// Did we drive?
+		assertEquals(0, testController.getCommand());
+		assertEquals(25, testController.getArgument());
+	}
 }
 
