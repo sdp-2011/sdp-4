3243c3ca4480c57fa5b3e90c120df019a27c8e06
Chris Brown <cb@tardis.ed.ac.uk>
2011-03-01 15:39:09 +0000

    Added a logger to the tactic.

--- computer/src/uk/ac/ed/inf/sdp/group4/strategy/tactic/PenaltyTakeTactic.java
+++ computer/src/uk/ac/ed/inf/sdp/group4/strategy/tactic/PenaltyTakeTactic.java
@@ -1,11 +1,15 @@
 package uk.ac.ed.inf.sdp.group4.strategy.tactic;
 
+import org.apache.log4j.Logger;
+
 import uk.ac.ed.inf.sdp.group4.controller.Controller;
 import uk.ac.ed.inf.sdp.group4.world.Ball;
 import uk.ac.ed.inf.sdp.group4.world.Robot;
 
 public class PenaltyTakeTactic extends Tactic
 {
+	protected static Logger log = Logger.getLogger(PenaltyTakeTactic.class);
+
 	public PenaltyTakeTactic(Controller controller)
 	{
 		this(controller, false);
